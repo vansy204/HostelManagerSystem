a942d3788b6ecb9edcbf3a8fd93fbdc307e7a250
vansy204 <phamvansy204@gmail.com>
2025-03-11 22:58:10 +0700

    Initial commit

---
+++ src/test/java/com/hostelmanagermentsystem/HostelManagermentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hostelmanagermentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostelManagermentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
