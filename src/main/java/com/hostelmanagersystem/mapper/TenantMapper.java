34aaa8a5a3bebd8cbda4038d096034df9f757845
VanAn <143919601+VanAn2004@users.noreply.github.com>
2025-05-17 13:47:49 +0700

    daidaidi

---
+++ src/main/java/com/hostelmanagersystem/mapper/TenantMapper.java
@@ -0,0 +1,16 @@
+package com.hostelmanagersystem.mapper;
+
+import com.hostelmanagersystem.dto.response.TenantResponse;
+import com.hostelmanagersystem.entity.manager.Tenant;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface TenantMapper {
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "roomId", target = "roomId")
+    @Mapping(source = "checkInDate", target = "checkInDate")
+    @Mapping(source = "checkOutDate", target = "checkOutDate")
+    @Mapping(source = "status", target = "status")
+    TenantResponse toResponse(Tenant tenant);
+}
