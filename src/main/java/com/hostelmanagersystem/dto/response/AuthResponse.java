4a27dc247e67c68ad4d2e6a27e81e2c4222b209b
VanAn <143919601+VanAn2004@users.noreply.github.com>
2025-05-25 00:13:49 +0700

    sua room

--- src/main/java/com/hostelmanagersystem/dto/response/AuthResponse.java
+++ src/main/java/com/hostelmanagersystem/dto/response/AuthResponse.java
@@ -9,6 +9,7 @@ import lombok.experimental.FieldDefaults;
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class AuthResponse {
+    String userName;
     String token;
     boolean authenticated;
 }
