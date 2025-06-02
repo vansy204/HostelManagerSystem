510a4e658c5ed4d4c86482667bda523aaa0c3091
phucloi <phucloidanghoconline@gmail.com>
2025-06-02 22:51:22 +0700

    fix some error

--- src/main/java/com/hostelmanagersystem/exception/ErrorCode.java
+++ src/main/java/com/hostelmanagersystem/exception/ErrorCode.java
@@ -52,9 +52,7 @@ public enum ErrorCode {
     INVALID_CHECKIN_DATE(1021,"Thời gian không hợp lệ", HttpStatus.UNAUTHORIZED ),
     LEASE_TERM_NOT_DEFINED(1022, "Phòng chưa cấu hình thời gian thuê tối thiểu", HttpStatus.UNAUTHORIZED),
     ROOM_NOT_IN_CLEANING(1023, "Phòng chưa đựoc dọn dẹp", HttpStatus.UNAUTHORIZED),
-
-    INVALID_REQUEST_STATUS(1024,"Trạng thái yêu cầu không hợp lệ", HttpStatus.BAD_REQUEST),
-  ;
+    ;
 
 
     int code;
